39ee45553cad4c692a12d07bd1552b86c352ed9d
SergeBorshevskiy <sk8er1001@mail.ru>
2022-03-22 16:15:31 +0600

    Creating resources and presentation fragments

---
+++ app/src/main/java/com/borshevskiy/cleanarchitecturesingleactivitycompositionofnumber/presentation/ChooseLevelFragment.kt
@@ -0,0 +1,28 @@
+package com.borshevskiy.cleanarchitecturesingleactivitycompositionofnumber.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.borshevskiy.cleanarchitecturesingleactivitycompositionofnumber.R
+import com.borshevskiy.cleanarchitecturesingleactivitycompositionofnumber.databinding.FragmentChooseLevelBinding
+
+class ChooseLevelFragment : Fragment() {
+
+    private var _binding: FragmentChooseLevelBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentChooseLevelBinding.inflate(layoutInflater,container,false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
