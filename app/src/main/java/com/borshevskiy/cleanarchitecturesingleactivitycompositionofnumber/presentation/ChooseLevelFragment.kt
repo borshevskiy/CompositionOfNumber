ae39f23b2c870424e29072c06033f74aab6a9d53
SergeBorshevskiy <sk8er1001@mail.ru>
2022-03-23 09:36:47 +0600

    Creating nav_graph, saveArgs and nav_actions

--- app/src/main/java/com/borshevskiy/cleanarchitecturesingleactivitycompositionofnumber/presentation/ChooseLevelFragment.kt
+++ app/src/main/java/com/borshevskiy/cleanarchitecturesingleactivitycompositionofnumber/presentation/ChooseLevelFragment.kt
@@ -1,12 +1,13 @@
 package com.borshevskiy.cleanarchitecturesingleactivitycompositionofnumber.presentation
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.borshevskiy.cleanarchitecturesingleactivitycompositionofnumber.R
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.borshevskiy.cleanarchitecturesingleactivitycompositionofnumber.databinding.FragmentChooseLevelBinding
+import com.borshevskiy.cleanarchitecturesingleactivitycompositionofnumber.domain.entity.Level
 
 class ChooseLevelFragment : Fragment() {
 
@@ -19,6 +20,22 @@ class ChooseLevelFragment : Fragment() {
     ): View {
         _binding = FragmentChooseLevelBinding.inflate(layoutInflater,container,false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.buttonLevelTest.setOnClickListener {
+            findNavController().navigate(ChooseLevelFragmentDirections.actionChooseLevelFragmentToGameFragment(Level.TEST))
+        }
+        binding.buttonLevelEasy.setOnClickListener {
+            findNavController().navigate(ChooseLevelFragmentDirections.actionChooseLevelFragmentToGameFragment(Level.EASY))
+        }
+        binding.buttonLevelNormal.setOnClickListener {
+            findNavController().navigate(ChooseLevelFragmentDirections.actionChooseLevelFragmentToGameFragment(Level.NORMAL))
+        }
+        binding.buttonLevelHard.setOnClickListener {
+            findNavController().navigate(ChooseLevelFragmentDirections.actionChooseLevelFragmentToGameFragment(Level.HARD))
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/borshevskiy/cleanarchitecturesingleactivitycompositionofnumber/presentation/GameFragment.kt
+++ app/src/main/java/com/borshevskiy/cleanarchitecturesingleactivitycompositionofnumber/presentation/GameFragment.kt
@@ -1,19 +1,30 @@
 package com.borshevskiy.cleanarchitecturesingleactivitycompositionofnumber.presentation
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.borshevskiy.cleanarchitecturesingleactivitycompositionofnumber.R
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.borshevskiy.cleanarchitecturesingleactivitycompositionofnumber.databinding.FragmentGameBinding
 
 class GameFragment : Fragment() {
+
+    private var _binding: FragmentGameBinding? = null
+    private val binding get() = _binding!!
+
+    private val args by navArgs<GameFragmentArgs>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game, container, false)
+    ): View {
+        _binding = FragmentGameBinding.inflate(layoutInflater,container,false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
